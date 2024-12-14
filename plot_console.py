b53344abf685361deb19e9f7ed37c272654adbe5
serge1611 <135701531+serge1611@users.noreply.github.com>
2024-12-14 20:44:09 +0100

    Update plot_console.py

--- plot_console.py
+++ plot_console.py
@@ -3,7 +3,7 @@ from typedefs import *
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import ks_1samp, shapiro, kstest, kruskal, norm
+from scipy.stats import ks_1samp, shapiro, kstest, kruskal, norm, spearmanr
 import scikit_posthocs as sp
 
 class DataContext:
